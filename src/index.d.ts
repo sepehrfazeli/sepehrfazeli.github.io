d44ef1003aa72ad63ed3b66a4272388acca7dbb6
Sepehr Fazeli <sky.f1118@gmail.com>
2022-06-24 19:30:35 +0200

    header and color changing

---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+declare module 'react-responsive'
+declare module 'react-share'
+declare module '*.svg'
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+
+
+declare namespace React {
+  interface MetaHTMLAttributes {
+    value?: string
+  }
+}
